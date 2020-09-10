64acee850167c1c85ade0c4a05c69dae5a45f241
sleepy81k <nabilnico@gmail.com>
2020-09-11 03:22:59 +0700

    First

---
+++ Latihan Array.Soal 2.js
@@ -0,0 +1,12 @@
+// **No.2
+// variabel
+var reactNative = ["Hafif", "Dayat", "Wandi"];
+var reactJs = ["Yusuf", "Lutfi", "Tompel"];
+
+// concat untuk menggabungkan bisa mengunakan spread jg (...)
+var JavaScriptDivision = reactNative.concat(reactJs);
+
+// cetak
+document.write("Javascript Division " + JavaScriptDivision);
+console.log("Javascript Division : " + JavaScriptDivision);
+console.log(JavaScriptDivision);
